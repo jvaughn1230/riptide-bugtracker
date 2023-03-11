8e2dbf00b61092ee72e8f126cc8aa319d30b8394
Alex <jvaughn1230@gmail.com>
2023-03-11 14:49:56 -0800

    added navbar with routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Layout";
+import Login from "./components/login/login";
+import AddBug from "./components/addBug/addBug";
+import ViewBugs from "./components/viewBugs/viewBugs";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/account" element={<Layout />}>
+          <Route path="/account/addbug" element={<AddBug />} />
+          <Route path="/account/viewbugs" element={<ViewBugs />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
